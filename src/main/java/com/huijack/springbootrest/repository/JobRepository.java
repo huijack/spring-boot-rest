8ef704b60410e1c78e939ff47f9815da0f974726
Huijack <jacklim2626@gmail.com>
2026-03-05 02:05:40 +0800

    Initial commit

---
+++ src/main/java/com/huijack/springbootrest/repository/JobRepository.java
@@ -0,0 +1,13 @@
+package com.huijack.springbootrest.repository;
+
+import com.huijack.springbootrest.model.JobPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface JobRepository extends JpaRepository<JobPost, Integer> {
+
+    List<JobPost> findByPostProfileContainingOrPostDescContaining(String postProfile, String postDesc);
+}
---
+++ src/test/java/com/huijack/springbootrest/SpringBootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huijack.springbootrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
